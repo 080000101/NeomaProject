790128d765e013fed083c4f9714eb16bc1d8617d
Aloe Vera <rimie.nkou.20@neoma-bs.com>
2021-12-02 14:49:14 +0100

    change defaultController into HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,28 +1,18 @@
 <?php
 
-// src/Controller/DefaultController.php
-
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\Routing\Annotation\Route;
 
-
-class DefaultController extends AbstractController
-
+class HomeController extends AbstractController
 {
     /**
-    * @Route("/", name="app_index")
+    * @Route("/", name="home_index")
     */
     public function index(): Response
     {
-
-            return $this->render('/index.html.twig');
-
-    } 
-
-} 
+        return $this->render('/index.html.twig');
+    }
+}
