83144a740d3afbcb47126cfda076ef55fadaac30
mauricia-10 <rimie.nkou.20@neoma-bs.com>
2022-01-06 02:24:34 +0100

    Finish CSS contact

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -8,19 +8,32 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\CountryType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ContactType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname',)
-            ->add('lastname')
-            ->add('phone_number', NumberType::class,)
-            ->add('email')
-            ->add('country', CountryType::class)
+            ->add('firstname', TextType::class,
+            ['attr' => ['class' => 'test',
+                        'placeholder' => 'Marie']
+            ],)
+            ->add('lastname', TextType::class,
+            ['attr' => ['class' => 'test',
+                        'placeholder' => 'Dupont']
+            ],)
+            ->add('phone_number', NumberType::class,
+            ['attr' => ['class' => 'test',
+                        'placeholder' => '0654378965']
+            ],)
+            ->add('email', TextType::class,
+            ['attr' => ['class' => 'test',
+                        'placeholder' => 'mariedupont@gmail.com']
+            ],)
+            /* ->add('country', CountryType::class)
             ->add('city')
-            ->add('adress')
+            ->add('adress')*/
             ->add('account')
         ;
     }
--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -6,6 +6,7 @@ use App\Entity\Account;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -17,11 +18,16 @@ class RegistrationFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add('email', TextType::class,
+                ['attr' => ['placeholder' => "nom d'utilisateur"]],
+            )
+
             ->add('plainPassword', RepeatedType::class, [
                 'type' => PasswordType::class,
                 'invalid_message' => 'Le mot de passe est incorrect.',
-                'options' => ['attr' => ['class' => 'password-field']],
+                'options' => ['attr' => ['class' => 'password-field',
+                                         'placeholder' => '*************']
+                             ],
                 'required' => true,
                 'first_options'  => ['label' => 'Mot de passe'],
                 'second_options' => ['label' => 'Repète ton mot de passe'],
