ea449fd1a8c480d95752ac4336cfb012d4556021
mauricia-10 <rimie.nkou.20@neoma-bs.com>
2022-01-24 10:43:46 +0100

    make crud email

---
+++ src/Form/EmailType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Email;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+
+class EmailType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,
+            ['attr' => ['class' => 'name-test',
+                        'placeholder' => 'Libellé']
+            ],)
+            ->add('email', EmailType::class,
+            ['attr' => ['class' => 'name-test',
+                        'placeholder' => 'Adresse email']
+            ],)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Email::class,
+        ]);
+    }
+}
