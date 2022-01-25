d9a3677b9258c598f559e5abb860156bf4a4eb12
mauricia-10 <rimie.nkou.20@neoma-bs.com>
2022-01-25 13:38:20 +0100

    phone_number crud working

---
+++ src/Form/PhoneNumberType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\PhoneNumber;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+
+
+class PhoneNumberType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class,
+            ['attr' => ['class' => 'name-test',
+                        'placeholder' => 'Libellé']
+            ],)
+            ->add('number', TelType::class,
+            ['attr' => ['class' => 'name-test',
+                        'placeholder' => 'Numéro de téléphone']
+            ],)
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => PhoneNumber::class,
+        ]);
+    }
+}
